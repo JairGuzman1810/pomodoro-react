c8090a65dd0e599bd1dea2a3f5d9e180aa741270
Jair1810 <jair18101@hotmail.com>
2023-12-19 11:13:38 -0600

    Header agregado Ya se cambian el fondo según cual cronometro se quiere utilizar. Empezando Timer.jsx

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Platform,
+  Text,
+  View,
+  Button,
+  SafeAreaView,
+} from "react-native";
+import { useState } from "react";
+import Header from "./src/components/Header";
+
+const colors = ["#F7D6BF", "#AFEEEE", "#88D8BD"];
 
 export default function App() {
+  const [isWorking, setIsWorking] = useState(false);
+  const [time, setTime] = useState(25 * 60);
+  const [currentTime, setCurrentTime] = useState("POMO" | "SHORT" | "BREAK");
+  console.log(currentTime);
+
   return (
-    <View style={styles.container}>
-      <Text>Test commit</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView
+      style={[styles.container, { backgroundColor: colors[currentTime] }]} //Mediante el colors[currentTime] se va cambiar el color segun el index
+    >
+      <View style={{ paddingTop: Platform.OS === "android" && 30 }}>
+        <Text style={styles.text}>Pomodoro</Text>
+        <Text style={styles.text}>{time}</Text>
+        <Header
+          currentTime={currentTime}
+          setCurrentTime={setCurrentTime}
+          setTime={setTime}
+        />
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  },
+  text: {
+    fontSize: 32,
+    fontWeight: "bold",
   },
 });
---
+++ src/components/Header.jsx
@@ -0,0 +1,43 @@
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+
+const options = ["Pomodoro", "Short Break", "Long Break"];
+export default function Header({ currentTime, setCurrentTime, setTime }) {
+  function handlePress(index) {
+    //Asi se asigna a una variable const un valor
+    //como es inmutable, entonces se debe realizar de esa forma
+    //:15 es como el else, si no es 0 o 1 entonces sera 2, entonces 15
+    const newTime = index === 0 ? 25 : index === 1 ? 5 : 15;
+
+    setCurrentTime(index);
+    setTime(newTime * 60);
+  }
+  return (
+    <View style={{ flexDirection: "row" }}>
+      {options.map((item, index) => (
+        <TouchableOpacity
+          key={index}
+          onPress={() => handlePress(index)}
+          style={[
+            style.itemStyle,
+            currentTime !== index && { borderColor: "transparent" }, //Asi se pone un estilo mediante una validación
+            // && significa el resultado
+          ]}
+        >
+          <Text style={{ fontWeight: "bold" }}>{item}</Text>
+        </TouchableOpacity>
+      ))}
+    </View>
+  );
+}
+
+const style = StyleSheet.create({
+  itemStyle: {
+    borderWidth: 3,
+    padding: 5,
+    width: "33%",
+    borderColor: "white",
+    marginVertical: 20,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+});
